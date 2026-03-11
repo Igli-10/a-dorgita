b3446e393f1345e8ad0a46e93fac34f8614dd621
Igli-10 <alejandroiglesiassantos@gmail.com>
2026-03-11 13:12:32 +0100

    Estrutura de navbar con categorías centradas e estilo Times New Roman

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="gl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,16 +8,27 @@
     <link rel="stylesheet" href="public/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/estilos.css">
     <link rel="icon" type="image/png" href="public/img/logo_favicon.png">
-    
+
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark fondo-verde">
+    <nav class="navbar navbar-dark fondo-verde">
         <div class="container-fluid">
             <a class="navbar-brand d-flex align-items-center" href="index.php">
-                <img src="public/img/logo.png" width="50" height="50" class="me-2">    A Dorgita
+                <img src="public/img/logo.png" width="50" height="50" class="d-inline-block align-text-top"> A Dorgita
             </a>
-    </div>
-</nav>
-                       
-
-
+            <div class="flex-grow-1 d-flex justify-content-center">
+                <ul class="nav ">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Alimentación</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ferretería</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Fogar</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>

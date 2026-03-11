9e94cd25c7af28de133d267f6be0cae66e1d6b5c
Igli-10 <alejandroiglesiassantos@gmail.com>
2026-03-11 08:55:10 +0100

    Creado o index.php, o header.php e engadido o favicon da tenda

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="gl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A Dorgita</title>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="icon" type="image/png" href="public/img/logo_favicon.png">
+</head>
+<body>
+    
+</body>
+</html>
+

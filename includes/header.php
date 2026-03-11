a4c5c5ea896f27d7780138361e7a83aa7a80b744
Igli-10 <alejandroiglesiassantos@gmail.com>
2026-03-11 10:02:38 +0100

    Deseño inicial: creación de estilos.css, fondo verde na navbar e integración do logo

--- includes/header.php
+++ includes/header.php
@@ -6,10 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>A Dorgita</title>
     <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/estilos.css">
     <link rel="icon" type="image/png" href="public/img/logo_favicon.png">
+    
 </head>
 <body>
-    
-</body>
-</html>
+    <nav class="navbar navbar-expand-lg navbar-dark fondo-verde">
+        <div class="container-fluid">
+            <a class="navbar-brand d-flex align-items-center" href="index.php">
+                <img src="public/img/logo.png" width="50" height="50" class="me-2">    A Dorgita
+            </a>
+    </div>
+</nav>
+                       
+
 

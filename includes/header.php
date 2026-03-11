767b7cd7321124680941bea3748cfb0f7ce0f5d6
Igli-10 <alejandroiglesiassantos@gmail.com>
2026-03-11 13:56:12 +0100

    Integración de Bootstrap Icons, organización do nav en bloques e limpeza de código HTML coa nova sección de contacto

--- includes/header.php
+++ includes/header.php
@@ -7,28 +7,38 @@
     <title>A Dorgita</title>
     <link rel="stylesheet" href="public/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/estilos.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="icon" type="image/png" href="public/img/logo_favicon.png">
 
 </head>
 
 <body>
-    <nav class="navbar navbar-dark fondo-verde">
-        <div class="container-fluid">
+  <nav class="navbar navbar-dark fondo-verde">
+    <div class="container-fluid d-flex align-items-center">
+        
+        <div style="width: 25%;">
             <a class="navbar-brand d-flex align-items-center" href="index.php">
-                <img src="public/img/logo.png" width="50" height="50" class="d-inline-block align-text-top"> A Dorgita
+                <img src="public/img/logo.png" width="50" height="50" class="me-2"> 
+                A Dorgita
             </a>
-            <div class="flex-grow-1 d-flex justify-content-center">
-                <ul class="nav ">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Alimentación</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Ferretería</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Fogar</a>
-                    </li>
-                </ul>
-            </div>
         </div>
-    </nav>
+
+        <div class="flex-grow-1 d-flex justify-content-center">
+            <ul class="nav">
+                <li class="nav-item"><a class="nav-link" href="#">Alimentación</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Ferretería</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Fogar</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Contacto</a></li>
+            </ul>
+        </div>
+
+        <div style="width: 25%;" class="d-flex justify-content-end">
+            <ul class="nav">
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-search"></i></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-cart3"></i></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-person"></i></a></li>
+            </ul>
+        </div>
+
+    </div>
+</nav>

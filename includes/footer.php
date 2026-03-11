1ff79cca85104b82afe6f4520d7e517844a51d25
Igli-10 <alejandroiglesiassantos@gmail.com>
2026-03-11 16:53:57 +0100

    3ª columna de contacto engadida e axuste do centrado xeral do footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,9 @@
 <footer class="fondo-verde mt-5 pt-5 pb-3 mt-auto">
     <div class="container">
-        <div class="row">
+        <div class="row text-center">
+
             <div class="col-md-4 mb-4">
-                <span class="d-flex align-items-center mb-3">
+                <span class="d-flex align-items-center  mb-3 justify-content-center">
                     <img src="public/img/logo.png" width="40" height="40" class="me-2">
                     <span class="fs-4 fw-bold">A Dorgita</span>
                 </span>
@@ -15,6 +16,12 @@
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Aviso Legal</a></li>
                     <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Política de Privacidade</a></li>
                 </ul>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <h5 class="mb-3 fw-bold">Contacto</h5>
+                <p class="mb-1">A Silva, Cerceda, Galicia</p>
+                <p>+34 981 123 456</p>
             </div>
         </div>
     </div>

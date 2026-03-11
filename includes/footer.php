bb99c15de76f92b9f1d82851b66ded209a5cfb70
Igli-10 <alejandroiglesiassantos@gmail.com>
2026-03-11 16:35:53 +0100

    Axuste de estilos CSS para texto branco e creación da primeira columna do footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,16 @@
-<footer class="fondo-verde mt-5" style="min-height: 100px;"></footer>
+<footer class="fondo-verde mt-5 pt-5 pb-3 mt-auto">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <span class="d-flex align-items-center mb-3">
+                    <img src="public/img/logo.png" width="40" height="40" class="me-2">
+                    <span class="fs-4 fw-bold">A Dorgita</span>
+                </span>
+                <p>Alimentación, ferretería e moito máis</p>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
+
 </html>
